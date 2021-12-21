0563ca67b65893403b1d825dd493b52224daba85
Alejandro Saucedo <axsauze@gmail.com>
2021-12-21 07:48:29 +0000

    Added initial ico

--- erc20-mlcoin-token.sol
+++ erc20-mlcoin-token.sol
@@ -71,3 +71,54 @@ contract MLCoinToken is ERC20Interface{
         return true;
     }
 }
+
+contract MLCoinTokenICO is MLCoinToken{
+    address public admin;
+    address payable public deposit;
+    uint tokenPrice = 0.001 ether; // 1ETH = 1000 MLCoinToken, 1CRPT = 0.001ETH
+    uint public hardCap = 300 ether;
+    uint public raisedAmount;
+    uint public saleStart = block.timestamp;
+    uint public saleEnd = block.timestamp + 604800; // ICO ends in a week
+    uint public tokenTradeStart = saleEnd + 604800; // Transferable in a week after sale
+    uint public maxInvestment = 5 ether;
+    uint public minInvestment = 0.1 ether;
+
+    enum State {beforeStart, running, afterEnd, halted}
+    State public icoState;
+
+    constructor(address payable _deposit){
+        deposit = _deposit;
+        admin = msg.sender;
+        icoState = State.beforeStart;
+    }
+
+    modifier onlyAdmin(){
+        require(msg.sender == admin);
+        _;
+    }
+
+    function halt() public onlyAdmin{
+        icoState = State.halted;
+    }
+
+    function resume() public onlyAdmin{
+        icoState = State.running;
+    }
+
+    function changeDepositAddress(address payable newDeposit) public onlyAdmin{
+        deposit = newDeposit;
+    }
+
+    function getCurrentState() public view returns(State){
+        if(icoState == State.halted){
+            return State.halted;
+        } else if(block.timestamp < saleEnd) {
+            return State.beforeStart;
+        } else if(block.timestamp >= saleStart && block.timestamp <= saleEnd) {
+            return State.running;
+        } else {
+            return State.afterEnd;
+        }
+    }
+}
